47e5704ba1a470db9c364d9ba89d2fd20f03d0c4
kamilDevguru <kamil.czoska1997@outlook.com>
2019-07-05 18:00:48 +0800

    Quote div

--- src/components/priceQuote/index.js
+++ src/components/priceQuote/index.js
@@ -1,16 +1,32 @@
 import React from "react"
-import { Button } from "react-bootstrap"
+import { Button, Row } from "react-bootstrap"
 
 import "./style.scss"
+import ActionMark from "./image/action-mark.png"
+import QuoteMark from "./image/clock-mark.png"
 
 const BreadCrumb = () => {
   return (
-    <div className="breadcrumb__container">
-      <a className="breadcrumb__link" href="#">Home</a>
-      <span className="breadcrumb__marker">›</span>
-      <a className="breadcrumb__link" href="#">Solutions</a>
-      <span className="breadcrumb__marker">›</span>
-      <a className="breadcrumb__link active" href="#">Wine</a>
+    <div className="price-quote__container">
+      <h1 className="price-quote__title">
+        Get your instant price quote
+      </h1>
+      <Row className="price-quote__item">
+        <img src={QuoteMark} alt="mark" />
+        <span className="price-quote__text">
+          Get a personalised quote in minutes
+        </span>
+      </Row>
+      <Row className="price-quote__item">
+        <img src={QuoteMark} alt="mark" />
+        <span className="price-quote__text">
+          Market leading prices
+        </span>
+      </Row>
+      <Button className="price-quote__action">
+        <img src={ActionMark} alt="mark" />
+        Request Quote
+      </Button>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,21 @@ import { Row, Col } from "react-bootstrap"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Hero from "../components/hero"
-import BreadCrumb from "../components/breadCrumb";
-import Intro from "../components/intro";
+import BreadCrumb from "../components/breadCrumb"
+import Intro from "../components/intro"
+import PriceQuote from "../components/priceQuote"
 
 const IndexPage = () => (
   <Layout pageInfo={{ pageName: "index" }}>
     <SEO title="Home" keywords={[`gatsby`, `react`, `bootstrap`]} />
     <Hero />
     <BreadCrumb />
-    <Row>
+    <Row className="index__row">
       <Col className="index__intro" lg={8} md={8} sm={12}>
         <Intro />
+      </Col>
+      <Col className="index__quote" lg={4} md={4} sm={12}>
+        <PriceQuote />
       </Col>
     </Row>
   </Layout>
